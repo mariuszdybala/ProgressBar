a06970467c679f05518dffe330c4590e6d1e6d34
mariuszdybala <dybala.mariusz@gmail.com>
2015-09-22 21:51:51 +0200

    My new ProgressBar window to long term method
    My new ProgressBar window to long term method

---
+++ App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DynamicSplashScreen
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+
+        //public static ISplashScreen splashScreen;
+
+        //private ManualResetEvent ResetSplashCreated;
+        //private Thread SplashThread;
+        //protected override void OnStartup(StartupEventArgs e)
+        //{
+        //    //// ManualResetEvent acts as a block.  It waits for a signal to be set.
+        //    //ResetSplashCreated = new ManualResetEvent(false);
+
+        //    //// Create a new thread for the splash screen to run on
+        //    //SplashThread = new Thread(ShowSplash);
+        //    //SplashThread.SetApartmentState(ApartmentState.STA);
+        //    //SplashThread.IsBackground = true;
+        //    //SplashThread.Name = "Splash Screen";
+        //    //SplashThread.Start();
+
+        //    //// Wait for the blocker to be signaled before continuing. This is essentially the same as: while(ResetSplashCreated.NotSet) {}
+        //    //ResetSplashCreated.WaitOne();
+
+        //    base.OnStartup(e);
+        //}
+
+        //private void ShowSplash()
+        //{
+        //    // Create the window
+        //    AnimatedSplashScreenWindow animatedSplashScreenWindow = new AnimatedSplashScreenWindow();
+        //    splashScreen = animatedSplashScreenWindow;
+
+        //    // Show it
+        //    animatedSplashScreenWindow.Show();
+
+        //    // Now that the window is created, allow the rest of the startup to run
+        //    ResetSplashCreated.Set();
+        //    System.Windows.Threading.Dispatcher.Run();
+        //}
+    }
+}
---
+++ LoadingClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DynamicSplashScreen
+{
+  public class LoadingClass
+    {
+        public static ISplashScreen splashScreen;
+        private ManualResetEvent resetSplashCreated;
+        private Thread splashTread;
+
+        public void Start()
+        {
+            resetSplashCreated = new ManualResetEvent(false);
+            splashTread = new Thread(ShowSplash);
+            splashTread.SetApartmentState(ApartmentState.STA);
+            splashTread.IsBackground = true;
+            splashTread.Name = "Splash Screen";
+            splashTread.Start();
+
+            resetSplashCreated.WaitOne();
+
+        }
+
+        public void End()
+        {
+            splashScreen.LoadComplete();
+        }
+
+
+        public void AddMessage(string message)
+        {
+            splashScreen.AddMessage(message);
+
+        }
+
+
+        private void ShowSplash()
+        {
+            ProgressWait animatedSplashScreenWindow = new ProgressWait();
+            splashScreen = animatedSplashScreenWindow;
+            animatedSplashScreenWindow.Show();
+
+            resetSplashCreated.Set();
+
+            System.Windows.Threading.Dispatcher.Run();
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
